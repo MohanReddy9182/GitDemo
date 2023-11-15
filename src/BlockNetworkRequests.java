611dbe4ca1ae94528de0976da47b61b5a873e418
mohan <reddyrockmohan@gmail.com>
2023-11-15 16:00:31 +0530

    second commit

--- src/BlockNetworkRequests.java
+++ src/BlockNetworkRequests.java
@@ -33,6 +33,8 @@ System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chro
 		System.out.println(driver.findElement(By.cssSelector("p")).getText());
 		long endTime = System.currentTimeMillis();
 		System.out.println(endTime - startTime);
+		System.out.println(endTime);
+		System.out.println(startTime);
 		//1793 2033
 		
 		
